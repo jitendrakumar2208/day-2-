397a48b2179e09732707fd6a21a4902b7140ba31
jitendrakumar2208 <erjitendrakumar2208@gmail.com>
2024-03-04 23:53:47 +0530

    done

---
+++ App.js
@@ -0,0 +1,18 @@
+var conn = (
+    <div class = "box">
+     <h1 class = "box">Topic Covered</h1>
+     <p>The Following is the list of all the  topic in the MDN Learning Area</p>
+     <br />
+
+     <p class = "box"><a href="http://">Getting Started with web</a></p>
+        <p>provides a practical Knowladge for web development to complere beginner</p>
+
+     <p class = "box"><a href="http://">HTML - Structuring the web</a> </p>
+     <p>HTMl is language that we used to structure the different parts of our content and define what their meaning or purpose is.</p>
+
+     <p class = "box"><a href="http://">CSS- Styling the web</a></p>
+     <p class = "box">CSS is language that we used to control of our content style and layout. as well ass adding behaiviour like animation.this topic provides comprehensive coverage of css</p>
+    </div>
+)
+
+ReactDOM.render(conn,document.getElementById("root"));
